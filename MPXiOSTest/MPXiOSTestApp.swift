16360253f21ceebb4b2747a9fa76087f753d3ca3
phucd <phuc.dinh@tasconline.com>
2023-09-25 08:38:19 -0500

    Initial commit

---
+++ MPXiOSTest/MPXiOSTestApp.swift
@@ -0,0 +1,34 @@
+//
+//  MPXiOSTestApp.swift
+//  MPXiOSTest
+//
+//  Created by Phuc Dinh on 5/3/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
